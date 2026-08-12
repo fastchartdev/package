5e03bcf15bef6d826c0708350ce9e8c89179e6a9
salah eddine bendyab <64494826+salahhusa9@users.noreply.github.com>
2026-08-12 19:24:05 +0100

    Implement recordEvent method to handle event creation and dispatching

--- src/Package.php
+++ src/Package.php
@@ -2,15 +2,48 @@
 
 namespace Fastchartdev\Package;
 
+use Fastchartdev\Package\Models\Event;
+use Fastchartdev\Package\Models\EventRecord;
+use Fastchartdev\Package\Enums\EventRecordStatus;
+use Fastchartdev\Package\Jobs\RecordEventJob;
+
 use Carbon\CarbonPeriod;
+use Fastchartdev\Package\Enums\EventRecordStatusEnum;
 
 class Package
 {
+    public function recordEvent(string $eventName, mixed $value, \DateTimeInterface|string $timestamp, mixed $scopeValue)
+    {
+        $event = Event::where('name', $eventName)
+            ->first();
+
+        if (!$event) {
+            $event = Event::create([
+                'name' => $eventName,
+            ]);
+        }
+
+        $eventRecord = EventRecord::create([
+            'event_id' => $event->id,
+            'value' => $value,
+            'timestamp' => $timestamp,
+            'scope_value' => $scopeValue,
+            'status' => EventRecordStatusEnum::PENDING,
+            'started_at' => null,
+            'completed_at' => null,
+            'failed_at' => null,
+        ]);
+
+        RecordEventJob::dispatch($eventRecord->id);
+
+        return $eventRecord;
+    }
+
     public function generateAtFromRange(\DateTimeInterface|string $startAt, \DateTimeInterface|string $endAt, string $periodType)
     {
         $period = CarbonPeriod::create(
             ($startAt),
-            '1 '.$periodType,
+            '1 ' . $periodType,
             ($endAt)
         );
 
